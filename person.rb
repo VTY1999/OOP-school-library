7918c8a9dc2d5d4126ad11e04899c58bb7d066f3
verissimo francis <verissimotoyin50@gmail.com>
2022-04-19 15:48:58 +0100

    Person class inherits from Nameable

--- person.rb
+++ person.rb
@@ -1,4 +1,6 @@
-class Person
+require_relative './interface'
+
+class Person < Nameable
   attr_accessor :name, :age
   attr_reader :id
 
@@ -7,6 +9,7 @@ class Person
     @name = name
     @age = age
     @parent_permission = parent_permission
+    super()
   end
 
   def can_use_services?
@@ -23,6 +26,3 @@ class Person
     @age >= 18
   end
 end
-
-barry = Person.new(15)
-print barry.can_use_services?
